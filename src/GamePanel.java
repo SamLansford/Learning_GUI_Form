c6bbb1835b48ca9c8aa38180a15a9fe2b88ba1c6
Sam Lansford <sam.j.lansford@gmail.com>
2021-12-14 22:13:16 -0600

    Feat: adding move to the game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -51,6 +51,16 @@ public class GamePanel extends JPanel  implements ActionListener {
         }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple(){
@@ -59,7 +69,22 @@ public class GamePanel extends JPanel  implements ActionListener {
     }
 
     public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
 
+        switch(direction) {
+            case 'U':
+            y[0] = y[0] - UNIT_SIZE;
+            case 'D':
+            y[0] = y[0] + UNIT_SIZE;
+            case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            case 'R':
+            x[0] = x[0] + UNIT_SIZE;
+
+        }
     }
 
     public void checkApple() {
