7482382d6684a4d8b713c4ba2db8ed74111d5774
Sam Lansford <sam.j.lansford@gmail.com>
2021-12-27 19:59:03 -0500

    Feat: fixing issues on movement and other stuff

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,15 +1,16 @@
 import javax.swing.*;
 
-public class GameFrame extends JFrame {
+public class GameFrame extends JFrame{
 
-    GameFrame() {
+    GameFrame(){
 
         this.add(new GamePanel());
-        this.setTitle("Snake Game");
+        this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
+
     }
 }
